e21da0d2b8baec27735e51012a62978058b6e5e3
Peter Malmgren <peter.malmgren@zapier.com>
2018-12-31 15:43:07 -0500

    Add an image builder library

---
+++ image/builder.go
@@ -0,0 +1,127 @@
+//
+// godot
+// https://github.com/pmalmgren/godot
+//
+// Copyright © 2018 Peter Malmgren <me@petermalmgren.com>
+// Distributed under the MIT License.
+// See README.md for details.
+//
+
+package image
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/jhoonb/archivex"
+)
+
+type imagebuilder interface {
+	ImageBuild(context.Context, io.Reader, types.ImageBuildOptions) (types.ImageBuildResponse, error)
+}
+
+// BuildDockerImage builds a Docker image from a context
+func BuildDockerImage(cli imagebuilder, contextPath string, tag string) error {
+	dockerBuildContext, err := os.Open(contextPath)
+	if err != nil {
+		return fmt.Errorf("Error opening build context tarfile: %v", err)
+	}
+	options := types.ImageBuildOptions{
+		SuppressOutput: false,
+		Remove:         true,
+		ForceRemove:    true,
+		PullParent:     true,
+		Tags:           []string{tag},
+		Dockerfile:     "Dockerfile",
+	}
+	buildResponse, err := cli.ImageBuild(context.Background(), dockerBuildContext, options)
+	if err != nil {
+		return fmt.Errorf("Error building Docker image: %v", err)
+	}
+	defer func() {
+		if err := buildResponse.Body.Close(); err != nil {
+			log.Printf("Error closing Docker build response body: %v", err)
+		}
+	}()
+
+	p := make([]byte, 512)
+	for {
+		n, err := buildResponse.Body.Read(p)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println(string(p[:n]))
+				break
+			}
+			return fmt.Errorf("Error reading from Docker build response: %v", err)
+		}
+		fmt.Println(string(p[:n]))
+	}
+
+	return nil
+}
+
+// isolateDockerfile moves a Dockerfile to its own temporary directory
+func isolateDockerfile(dockerfilePath string) (string, error) {
+	dockerfileContents, err := ioutil.ReadFile(dockerfilePath)
+	if err != nil {
+		return "", fmt.Errorf("Error reading from Dockerfile: %v", err)
+	}
+
+	tmpDir, err := ioutil.TempDir("/tmp/", "dockerfile-directory")
+	if err != nil {
+		return "", fmt.Errorf("Error creating temporary directory: %v", err)
+	}
+
+	newDockerfilePath := fmt.Sprintf("%s/Dockerfile", tmpDir)
+	err = ioutil.WriteFile(newDockerfilePath, []byte(dockerfileContents), 0666)
+	if err != nil {
+		return "", fmt.Errorf("Error writing to Docker build context file: %v", err)
+	}
+
+	return tmpDir, nil
+}
+
+// BuildDockerContext adds directories and a Dockerfile to a tarball.
+// The caller is responsible for cleanup.
+func BuildDockerContext(dockerfilePath string, dirs ...string) (string, error) {
+	// move the Dockerfile to its own path
+	newDockerfilePath, err := isolateDockerfile(dockerfilePath)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err := os.RemoveAll(newDockerfilePath); err != nil {
+			log.Printf("Error removing Dockerfile path: %v", err)
+		}
+	}()
+
+	tar := new(archivex.TarFile)
+	tmpDir, err := ioutil.TempDir("/tmp", "")
+	if err != nil {
+		return "", fmt.Errorf("Error creating a temporary directory: %v", err)
+	}
+	tarPath := fmt.Sprintf("%s/buildcontext.tar", tmpDir)
+	if err := tar.Create(tarPath); err != nil {
+		log.Printf("Error creating Docker build context tarfile: %v", err)
+	}
+	if err := tar.AddAll(newDockerfilePath, false); err != nil {
+		return "", fmt.Errorf("Error adding Dockerfile to build context: %v", err)
+	}
+
+	for _, dir := range dirs {
+		if err := tar.AddAll(dir, true); err != nil {
+			return "", fmt.Errorf("Error adding directory %s to build context: %v", dir, err)
+		}
+	}
+
+	if err := tar.Close(); err != nil {
+		return "", fmt.Errorf("Error closing Docker build context file: %v", err)
+	}
+
+	return tarPath, nil
+}
